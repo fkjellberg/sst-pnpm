68b8e3c55e098ed222a9fd9cdacc6711dd81bacb
Fredrik Kjellberg <fredrik.kjellberg@gmail.com>
2022-01-24 09:42:34 +0100

    Add custom response header with a generated uuid v4

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,9 +1,13 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import * as uuid from "uuid";
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   return {
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
+    headers: {
+      "Content-Type": "text/plain",
+      "X-Request-ID": uuid.v4(),
+    },
     body: `Hello, World! Your request was received at ${event.requestContext.time}.`,
   };
 };
